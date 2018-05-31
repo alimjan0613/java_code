be6b293c56338d0476c02306ef77e734584680a6
Alimjan <Alimjan@172.18.104.88>
2018-05-31 17:54:37 -0400

    pop ups handeling

---
+++ MyProjects/src/test/java/com/my/selenium/projects/PopupsHandling.java
@@ -0,0 +1,14 @@
+package com.my.selenium.projects;
+
+import java.awt.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
